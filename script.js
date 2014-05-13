7726364e722308997c3e796c634a816b00e80bd6
shasharala <shasharala@outlook.com>
2014-05-12 18:55:06 -0700

    javascript for IE8 update request

--- script.js
+++ script.js
@@ -1,12 +1,40 @@
-/*
-pass this function the number of the container, tempContainer[n], it is to overwrite
-containers are to proceed in order
-e.g. <div id ="tempContainer0">, <div id="tempContainer1">
-use rememberMe to return the innerHTML of the container, tempContainer[n], to a var for safekeeping
-*/
-function remove(a) {
-	var b = "tempContainer" + a;
-	var rememberMe = document.getElementById(b).innerHTML;
-	document.getElementById(b).innerHTML = "";
-	return rememberMe;
+//COOKIES
+//all cookies from shasharala.tk begin with "shasha_ automagically"
+function checkCookie(name, value) {
+	if(getCookie(name) == value) {
+		return true;
+	}
+	else {
+		return false;
+	}
+}
+
+//all cookies from shasharala.tk begin with "shasha_ automagically"
+function getCookie(name) {
+	var cookieStr = document.cookie;
+	var returnValue = "";
+	var searchFor = "shasha_" + name + "=";
+	var startIndex = cookieStr.indexOf(searchFor)
+	if(startIndex != -1) {
+		startIndex += searchFor.length;
+		endIndex = cookieStr.indexOf(";", startIndex)
+		if (endIndex == -1) {
+			endIndex = cookieStr.length;
+  			returnValue = cookieStr.substring(startIndex, endIndex)
+  		}
+  	}
+  	return returnValue;
+}
+
+//all cookies from shasharala.tk begin with "shasha_ automagically"
+function setCookie(name, value, expires) {
+	var d = new Date();
+	d.setTime(d.getTime() + (expires*24*60*60*1000));
+	var e = "expires " + d.toUTCString();
+	document.cookie = "shasha_" + name + "=" + value + "; " + e;
+}
+
+//UNIVERSAL
+function hide(name) {
+	document.getElementById(name).style.display = "none";
 }
