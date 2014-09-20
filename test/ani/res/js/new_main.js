87bcc7e153944f0f17a2134beecc43780b47ceee
shasharala <shasharala@outlook.com>
2014-09-20 06:11:49 -0700

    more animation fiddling. Walk cycle on keypress is almost completed!

--- test/ani/res/js/new_main.js
+++ test/ani/res/js/new_main.js
@@ -1,11 +1,69 @@
-(function () {
+(function gameEngine() {
   var avatar,
       avatarImg,
+      twinJacks,
+      twinJacksImg,
       canvas;
 
-  function gameLoop () {
+  function onKeyUp(evt) {
+    /*//left
+    if (evt.keyCode === 37) {
+      avatar.update();
+      avatar.srcY = 0;
+      avatar.destX -= avatar.stepSize;
+    }
+    //up
+    else if (evt.keyCode === 38) {
+      avatar.update();
+      avatar.srcY = 100;
+      avatar.destY -= avatar.stepSize;
+    }
+    //right
+    else if (evt.keyCode === 39) {
+      avatar.update();
+      avatar.srcY = 50;
+      avatar.destX += avatar.stepSize;
+    }
+    //down
+    else if (evt.keyCode === 40) {
+      avatar.update();
+      avatar.srcY = 150;
+      avatar.destY += avatar.stepSize;
+    }*/
+  }
+
+  function onKeyDown(evt) {
+    //left
+    if (evt.keyCode === 37) {
+      avatar.srcY = 0;
+      avatar.update();
+      avatar.destX -= avatar.stepSize;
+    }
+    //up
+    else if (evt.keyCode === 38) {
+      avatar.update();
+      avatar.srcY = 100;
+      avatar.destY -= avatar.stepSize;
+    }
+    //right
+    else if (evt.keyCode === 39) {
+      avatar.update();
+      avatar.srcY = 50;
+      avatar.destX += avatar.stepSize;
+    }
+    //down
+    else if (evt.keyCode === 40) {
+      avatar.update();
+      avatar.srcY = 150;
+      avatar.destY += avatar.stepSize;
+    }
+  }
+
+  function gameLoop() {
     window.requestAnimationFrame(gameLoop);
-    avatar.update();
+    canvas.getContext("2d").clearRect(0, 0, canvas.width, canvas.height);
+    twinJacks.update();
+    twinJacks.render();
     avatar.render();
   }
 
@@ -17,9 +75,13 @@
       numberOfFrames = options.numberOfFrames || 1;
 
     that.context = options.context;
+    that.srcY = options.srcY || 0;
+    that.destX = options.destX || 0;
+    that.destY = options.destY || 0;
     that.width = options.width;
     that.height = options.height;
     that.image = options.image;
+    that.stepSize = options.stepSize || 0;
 
     that.update = function update() {
       tickCount += 1;
@@ -35,16 +97,15 @@
     };
 
     that.render = function render() {
-      that.context.clearRect(0, 0, that.width, that.height);
       that.context.drawImage(
       that.image,
-      frameIndex * that.width / numberOfFrames,
-      0,
-      that.width / numberOfFrames,
+      frameIndex * that.width,
+      that.srcY,
+      that.width,
       that.height,
-      0,
-      0,
-      that.width / numberOfFrames,
+      that.destX,
+      that.destY,
+      that.width,
       that.height);
     };
 
@@ -56,117 +117,35 @@
   canvas.height = 500;
 
   avatarImg = new Image();
+  twinJacksImg = new Image();
 
   avatar = sprite({
-  context: canvas.getContext("2d"),
-  width: 234,
-  height: 25,
-  image: avatarImg,
-  numberOfFrames: 10,
-  ticksPerFrame: 4
+    context: canvas.getContext("2d"),
+    srcY: 0,
+    destX: 250,
+    destY: 250,
+    width: 36, //width of sprite
+    height: 48, //height of sprite
+    image: avatarImg,
+    numberOfFrames: 8,
+    ticksPerFrame: 2,
+    stepSize: 5
   });
 
+  twinJacks = sprite({
+    context: canvas.getContext("2d"),
+    srcY: 0,
+    destX: 100,
+    destY: 100,
+    width: 162, //width of sprite
+    height: 56, //height of sprite
+    image: twinJacksImg,
+    numberOfFrames: 2,
+    ticksPerFrame: 50,
+  });
   avatarImg.addEventListener("load", gameLoop);
-  avatarImg.src = "res/img/rabbitLink.png";
+  document.addEventListener("keydown", onKeyDown);
+  document.addEventListener("keyup", onKeyUp);
+  avatarImg.src = "res/img/link.png";
+  twinJacksImg.src = "res/img/twinJacks.png";
 }());
-
-
-
-
-
-
-
-
-
-var sx = 0,
-    sy = 0,
-    sw = 16,
-    sh = 25,
-    dx = 100,
-    dy = 100,
-    dw = 16,
-    dh = 25,
-    pdx = 0,
-    pdy = 0,
-    stepSize = 10;
-function step(){
-  canvas.clearRect(pdx,pdy,dw,dh);
-  canvas.drawImage(avatar,sx,sy,sw,sh,dx,dy,dw,dh);
-}
-function stepL(){
-  if(dx<=0){
-    pdx = dx;
-    pdy = dy;
-    sx = 220;
-    window.requestAnimationFrame(step);
-  }
-  else{
-
-    pdx = dx;
-    pdy = dy;
-    dx -= stepSize;
-    sx = 220;
-    window.requestAnimationFrame(step);
-  }
-}
-function stepR(){
-  if(dx>=280){
-    pdx = dx;
-    pdy = dy;
-    sx = 0;
-    window.requestAnimationFrame(step);
-  }
-  else{
-    pdx = dx;
-    pdy = dy;
-    dx += stepSize;
-    sx = 0;
-    window.requestAnimationFrame(step);
-  }
-}
-function stepU(){
-  if(dy<=0){
-    pdx = dx;
-    pdy = dy;
-    sx = 121;
-    window.requestAnimationFrame(step);
-  }
-  else{
-    pdx = dx;
-    pdy = dy;
-    dy -= stepSize;
-    sx = 121;
-    window.requestAnimationFrame(step);
-  }
-}
-function stepD(){
-  if(dy>=120){
-    pdx = dx;
-    pdy = dy;
-    sx = 48;
-    window.requestAnimationFrame(step);
-  }
-  else{
-    pdx = dx;
-    pdy = dy;
-    dy += stepSize;
-    sx = 48;
-    window.requestAnimationFrame(step);
-  }
-}
-function onKeyDown(evt){
-  if(evt.keyCode==39){
-    stepR();
-  }
-  else if(evt.keyCode==37){
-    stepL();
-  }
-  else if(evt.keyCode==38){
-    stepU();
-  }
-  else if(evt.keyCode==40){
-    stepD();
-  }
-}
-
-document.addEventListener("keydown", onKeyDown);
