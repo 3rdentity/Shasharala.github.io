e7a1cc901a97bc76c1a551fc29a19563158c78cf
shasharala <shasharala@outlook.com>
2015-02-25 21:48:01 -0800

    new ideas and ui stuff

--- games/unfin/oneWord/res/js/main.js
+++ games/unfin/oneWord/res/js/main.js
@@ -1,8 +1,12 @@
-//REQUIREMENTS FOR GAME
-//A finite state machine that points at the current level. The current level will point at an array inside an array. The outer array will ce the container for every level in the game.
-//The inner arrays will contain the requirements/questions and the corresponding pattern/answer that needs to be passed.
-//Upon sucessful completion of a level the finite state machine ticks over to the next level, passing in the next array's information and pattern.
-//Each level will have its pattern passed to a testing function that will attempt to match the pattern to the player's input.
+/*
+*#########
+*TEMPLATES
+*#########
+*/
+
+var temp_login = '<div id="titleBar">Welcome to SuperSecureSite<sup>TM</sup></div><form id="dataBox" onsubmit="test()"><p id="prompt">sign in to continue</p><br><input id="username" class="dataEntry" type="text" placeholder="username" autocomplete="off" autofocus required><br><br><input id="password" class="dataEntry" type="text" placeholder="password" autocomplete="off" required><br><br><img id="badLoginFace" src="res/img/incorrFace.png" width="50px" draggable="false"><span id="badLogin">incorrect username<br>or password</span><p id="forgotPass" onclick="startStupid()"><u>forgot password?</u></p></form>';
+var temp_stupid = '<div id="titleBar">SuperSecureSite<sup>TM</sup> Authentication</div><form id="dataBox" onsubmit="test()"><p id="prompt">You have forgotten your password.<br>How unfortunate.<br>Please prove that you are a human by answering the following question.</p><div id="req"></div><br><br><input class="dataEntry" id="stupidAns" type="text" placeholder="i do not know" autocomplete="off" autofocus required></form>';
+var temp_pwd = '<div id="titleBar">SuperSecureSite<sup>TM</sup> Authentication</div><form id="dataBox" onsubmit="test()"><p id="prompt">Further authentication is required.<br>Create a password following the rules below.</p><div id="req"></div><br><br><br><input id="pwd" class="dataEntry" type="text" placeholder="w0rd5" autocomplete="off" required></form>';
 
 /*
 *####
@@ -31,7 +35,17 @@ var Fsm = {
   }
 };
 
+var states = {
+    events: [
+      { name: "login", from: "booting", to: "stupidQues", action: function readyAction() { Engine.run(); } },
+      { name: "pause", from: "playing", to: "paused", action: function pauseAction() { /*pause screen here*/ } },
+      { name: "unPause", from: "paused", to: "playing", action: function unPauseAction() { /*remove pause screen here*/ } }
+    ]
+}
 
+// needs to interact with FSM to respond with correct funcs
+// IE: first state is the login state. during login user cannot input
+//any data and must click "forgot password" to continue
 function test(val) {
     var data = document.getElementById("dataEntry").value;
     if(data === "one word") {
@@ -45,4 +59,8 @@ function test(val) {
         document.getElementById("theLegend").innerHTML = "INCORRECT";
     }
     event.preventDefault();
+}
+
+function startStupid() {
+    document.body.innerHTML = temp_stupid;
 }
