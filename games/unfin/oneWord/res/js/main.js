5afba7f3964e1aa717bce58cf6c80498cd3ddf22
shasharala <shasharala@outlook.com>
2015-03-01 07:01:55 -0800

    messymessymessy

--- games/unfin/oneWord/res/js/main.js
+++ games/unfin/oneWord/res/js/main.js
@@ -3,10 +3,11 @@
 *VARIABLES
 *#########
 */
-
+var gozillaClick = false;
 var newMailRead = false;
 var theDate = new Date();
-var theYear = theDate.getFullYear();
+var theYear = "" + theDate.getFullYear();
+var theShortYear = theYear.substring(2);
 var theMonth = theDate.getMonth() + 1;
 var theDay = theDate.getDate();
 
@@ -19,7 +20,7 @@ var temp_login = '<div id="titleBar">Welcome to SuperSecureSite<sup>TM</sup></di
 var temp_stupid = '<div id="titleBar">SuperSecureSite<sup>TM</sup> Authentication</div><form id="dataBox" onsubmit="test()"><p id="prompt">You have forgotten your password.<br>How unfortunate.<br>Please prove that you are a human by answering the following question.</p><div id="req"></div><br><br><input class="dataEntry" id="stupidAns" type="text" placeholder="i do not know" autocomplete="off" autofocus required></form>';
 var temp_pwd = '<div id="titleBar">SuperSecureSite<sup>TM</sup> Authentication</div><form id="dataBox" onsubmit="test()"><p id="prompt">Further authentication is required.<br>Create a password following the rules below.</p><div id="req"></div><br><br><br><input id="pwd" class="dataEntry" type="text" placeholder="w0rd5" autocomplete="off" required></form>';
 var temp_thanks = '<div id="titleBar">SSS<sup>TM</sup> Password Request</div><form id="dataBox"><p id="prompt">Thank you!<br><br>An e-mail has been sent to the e-mail address last associated with your current IP Address.<br><br>If you have any questions or do not receive an e-mail you must visit your nearest SSS establishment for further assistance.</p></form>';
-var temp_expired = '';
+var temp_expired = '<div id="mainInterface"><div id="titleBar">SSS<sup>TM</sup> Password Reset Expired</div><form id="dataBox" onsubmit="test()"><p id="prompt">It appears that this session has expired.<br><br>We apologize for any inconvenience this has caused you.<br><br>Please <a id="resetLink" onclick="resetGame()">start a new session</a>.<br><br>If you need assistance or have any questions please visit the closest SSS establishment to you.</p></form></div>';
 var temp_tempEmails = '';
 var temp_email = '<img id="eCount" src="res/img/eCount.png"><span id="emailSys">Gozilla Lightningbird<sup>TM</sup></span><hr>';
 
@@ -28,22 +29,27 @@ var temp_email = '<img id="eCount" src="res/img/eCount.png"><span id="emailSys">
 *EMAILS
 *######
 */
+/*+ (((theDay - 1) >= 1) ? (theMonth + "/" + (theDay - 1) + "/" + theShortYear) : ((theMonth - 1) + "/" + (theDay + 1) + "/" + theShortYear)) +*/
 var emails = [
-    "<div class='emailContent'><img id='backBtn' onclick='emailBack()' src='res/img/back.png'>&nbsp;<span class='emailContentHeader'><b>Subject:</b> \"IMPORTANT: SSS Password Reset\" <b>From:</b> SSS <b>Date:</b> " + theMonth + "/" + theDay + "</span><hr>Dear userNameHere,<br><br>You are receiving this e-mail because someone (Was it you?) requested a password reset from the IP Address associated with this e-mail in our records.<br>If you did not request a password reset then please visit the closest SSS establishment to you immediatly, your account may be vulnerable.<br><br>If you did request a password resest please click <a id='passResetLink' onclick='startExpired()'>here</a>.</div>",
-    "<div class='emailContent'><img id='backBtn' onclick='emailBack()' src='res/img/back.png'>&nbsp;<span class='emailContentHeader'><b>Subject:</b> \"What's Up?\" <b>From:</b> Robbin <b>Date:</b> " + theMonth + "/" + (((theDay - 2) >=1) ? (theDay - 2) : 1) + "</span><hr>Helllooo, anyone in there?<br>I haven't heard from you in a while!<br>What are you up to? Do you not like me anymore or something? I know you're busy with your \"projects\" or whatever, but we havn't hung out in ages! What's up with that?<br>Let's go out somewhere. I don't care where. Let's break something, do something, talk, c'mon, anything! I miss you!<br><br>Call me or something, ASAP, pleaasse?</div>",
-    "<div class='emailContent'><img id='backBtn' onclick='emailBack()' src='res/img/back.png'>&nbsp;<span class='emailContentHeader'><b>Subject:</b> \"Your Assistance is Needed\" <b>From:</b> Dr. William Hamford <b>Date:</b> " + theMonth + "/" + (((theDay - 3) >= 1) ? (theDay - 3) : 1) + "</span><hr>Good Day,<br><br>My name is Dr William Hamford, a staff in the Private Clients Section of a well-known bank, here in London, England. One of our accounts, with holding balance of £15,000,000 (Fifteen Million Pounds Sterling) has been dormant and last operated three years ago. From my investigations and confirmation, the owner of the said account, a foreigner by the name of Joe Shmoedja died in "+ month2Month(theMonth) + ", " + (theYear - 3) +" in a plane crash in Bristol.<br><br>Since then, nobody has done anything as regards the claiming of this money, as he has no family member that has any knowledge as to the existence of either the account or the funds; and also Information from the National Immigration also states that he was single on entry into the UK.<br>I have decided to find a reliable foreign partner to deal with. I therefore propose to do business with you, standing in as the next of kin of these funds from the deceased and funds released to you after necessary processes have been followed.<br><br>This transaction is totally free of risk and troubles as the fund is legitimate and does not originate from drug, money laundry, terrorism or any other illegal act.<br>On your interest, let me hear from you URGENTLY.<br><br>Best Regards,<br>Dr William Hamford, Financial Analysis and Remittance Manager</div>",
-    "<div class='emailContent'><img id='backBtn' onclick='emailBack()' src='res/img/back.png'>&nbsp;<span class='emailContentHeader'><b>Subject:</b> \"LadyKiller!!!\" <b>From:</b> Matthew <b>Date:</b> " + theMonth + "/" + (((theDay - 1) >= 1) ? (theDay - 1) : 1) + "</span><hr>Hey!<br><br>Have you seen <a href='https://www.youtube.com/watch?v=fEW2pdZ0qEE' target='_blank'>this</a> game? It looks amazing!<br>From what I have read about it it seems like it is not only particularly risqu&eacute;, hehe, but it will be very different from traditional visual novels! You know how in most visual novels wooing a specific chracter is done by doing things specifically for the character? Filling them up with charity like a vending machine till their adoration for you falls out? Well, this game ain't gonna take any of that shit from you. Your manipulation will have consequences.<br><br>Sounds exciting, huh?</div>",
+    ["IMPORTANT: SSS Password Reset", "SSS", 0, "Dear userNameHere,<br><br>You are receiving this e-mail because someone (Was it you?) requested a password reset from the IP Address associated with this e-mail in our records.<br>If you did not request a password reset then please visit the closest SSS establishment to you immediatly, your account may be vulnerable.<br><br>If you did request a password resest please click <a id='passResetLink' onclick='startExpired()'>here</a>."],
+    ["What's Up?", "Robbin", -2, "Helllooo, anyone in there?<br>I haven't heard from you in a while!<br>What are you up to? Do you not like me anymore or something? I know you're busy with your \"projects\" or whatever, but we havn't hung out in ages! What's up with that?<br>Let's go out somewhere. I don't care where. Let's break something, do something, talk, c'mon, anything! I miss you!<br><br>Call me or something, ASAP, pleaasse?"],
+    ["Your Assistance is Needed", "Dr. William Hamford", -3, "Good Day,<br><br>My name is Dr William Hamford, a staff in the Private Clients Section of a well-known bank, here in London, England. One of our accounts, with holding balance of £15,000,000 (Fifteen Million Pounds Sterling) has been dormant and last operated three years ago. From my investigations and confirmation, the owner of the said account, a foreigner by the name of Joe Shmoedja died in "+ month2Month(theMonth) + ", " + (theYear - 3) +" in a plane crash in Bristol.<br><br>Since then, nobody has done anything as regards the claiming of this money, as he has no family member that has any knowledge as to the existence of either the account or the funds; and also Information from the National Immigration also states that he was single on entry into the UK.<br>I have decided to find a reliable foreign partner to deal with. I therefore propose to do business with you, standing in as the next of kin of these funds from the deceased and funds released to you after necessary processes have been followed.<br><br>This transaction is totally free of risk and troubles as the fund is legitimate and does not originate from drug, money laundry, terrorism or any other illegal act.<br>On your interest, let me hear from you URGENTLY.<br><br>Best Regards,<br>Dr William Hamford, Financial Analysis and Remittance Manager"],
+    ["Ladykiller!!!", "Matthew", -1, "Hey!<br><br>Have you seen <a href='https://www.youtube.com/watch?v=fEW2pdZ0qEE' target='_blank'>this</a> game? It looks amazing!<br>From what I have read about it it seems like it is not only particularly risqu&eacute;, hehe, but it will be very different from traditional visual novels! You know how in most visual novels wooing a specific chracter is done by doing things specifically for the character? Filling them up with charity like a vending machine till their adoration for you falls out? Well, this game ain't gonna take any of that shit from you. Your manipulation will have consequences.<br><br>Sounds exciting, huh?"]
 ];
+
+var usedEmails = [];
 
 /*
 *################
 *STUPID QUESTIONS
 *################
 */
-var stupidQuestions = [
+var stupidQues = [
     ["What is the book number in which Albus Dumbledore of the Harry Potter series dies?", "6", "six"],
     ["", ""]
 ];
+
+var usedStupidQues = "";
 
 /*
 *################
@@ -54,6 +60,148 @@ var passPuzzles = [
     "",
     ""
 ];
+
+var usedPassPuzz = "";
+
+/*
+*####
+*DOM#
+*####
+*/
+var Dom = {
+  type: function domType(obj) {
+    return Object.prototype.toString.call(obj);
+  },
+  searchStr: function domSearchString(str, name) {
+    return str.indexOf(name);
+  },
+  get: function domGet(id) {
+    return document.getElementById(id);
+  },
+  set: function domSet(elem, html) {
+    elem.innerHTML = html;
+  },
+  hasClass: function domHasClass(elem, name) {
+    return (' ' + elem.className + ' ').indexOf(' ' + name + ' ') != -1;
+  },
+  addClass: function domAddClass(elem, name) {
+    elem.toggleClassName(name, true);
+  },
+  remClass: function domRemClass(elem, name) {
+    elem.toggleClassName(name, false);
+  },
+  toggleClass: function domToggleClass(elem, name, on) {
+    var classes = elem.className.split(' ');
+    var n = classes.indexOf(name);
+    on = (typeof on == "undefined") ? (n < 0) : on;
+    if (on && (n < 0)) classes.push(name);
+    else if (!on && (n >= 0)) classes.splice(n, 1);
+    elem.className = classes.join(' ');
+  },
+  hasAttr: function domHasAttr(elem, attr) {
+    return elem.hasAttribute(attr);
+  },
+  getAttr: function domGetAttr(elem, attr) {
+    return elem.getAttribute(attr);
+  },
+  addAttr: function domAddAttr(elem, attr, val) {
+    elem.setAttribute(attr, val);
+  },
+  remAttr: function domRemAttr(elem, attr) {
+    elem.removeAttribute(attr);
+  },
+  query: function domQuery(selector, context) {
+    return (context || document).querySelectorAll(selector);
+  },
+  on: function domOn(on, elem, type, fn, capture) {
+    capture = (typeof capture == "undefined") ? false : capture;
+    if(on === "elem") document.getElementById(elem).addEventListener(type, fn, capture);
+    else if(on === "obj") elem.addEventListener(type, fn, capture);
+  },
+  un: function domUn(elem, type, fn, capture) {
+    capture = (typeof capture == "undefined") ? false : capture;
+    document.getElementById(elem).removeEventListener(type, fn, capture);
+  },
+  // add "[hidden] {display: none;}" to CSS as a fallback
+  hide: function domHide(elem) {
+    this.addAttr(elem, "hidden");
+  },
+  show: function domShow(elem) {
+    this.remAttr(elem, "hidden");
+  },
+  replace: function domReplace(name1, name2) {
+    this.hide(name1);
+    this.show(name2);
+  },
+  cancelFade: function domCancelFade(elem) {
+    if (elem.fading) delete elem.fading;
+  },
+  fadeOut: function domFadeOut(elem, time) {
+    var fadeTime = 16;
+    if (time) fadeTime = time;
+    var opacity = 1;
+    elem.fading = undefined;
+    elem.style.opacity = 1;
+    elem.style.filter = "";
+    var last = new Date.getTime();
+    var tick = function fadeOutTick() {
+      opacity -= (new Date.getTime() - last) / 400;
+      elem.style.opacity = opacity;
+      elem.style.filter = "alpha(opacity=" + (100 * opacity)|0 + ")";
+      last = new Date.getTime();
+      (opacity > 0 && elem.fading) ? setTimeout(tick, fadeTime) : this.hide(elem);
+    };
+    tick();
+  },
+  fadeIn: function domFadeIn(elem, time) {
+    var fadeTime = 16;
+    if (time) fadeTime = time;
+    var opacity = 0;
+    elem.fading = undefined;
+    elem.style.opacity = 0;
+    elem.style.filter = "";
+    var last = new Date.getTime();
+    var tick = function fadeInTick() {
+      opacity += (new Date.getTime() - last) / 400;
+      elem.style.opacity = opacity;
+      elem.style.filter = "alpha(opacity=" + (100 * opacity)|0 + ")";
+      last = new Date.getTime();
+      if (opacity < 1 && elem.fading) setTimeout(tick, fadeTime);
+    };
+    this.show(elem);
+    tick();
+  },
+  replaceFade: function domReplaceFade(elem1, elem2, time1, time2) {
+    //fadeIn & fadeOut will handle time1 & time2 if they are not specified
+    this.fadeOut(elem1, time1);
+    this.fadeIn(elem2, time2);
+  },
+  // props may be overwrit and modifying extended obj will affect orig obj
+  // caveats:
+  // - objects from other frames/pages will be copied by reference, because their version of Object will be different
+  // - objects with a cyclic structure, will be traversed forever and overflow the JS stack
+  extend: function domExtend(dest, source) {
+    for (var prop in source)
+      dest[prop] = source[prop];
+    return dest;
+  },
+  // deepExtend will extend the object so that props are not overwrit and modifying extended obj won't affect orig obj
+  // caveats:
+  // - objects from other frames/pages will be copied by reference, because their version of Object will be different
+  // - objects with a cyclic structure, will be traversed forever and overflow the JS stack
+  deepExtend: function domDeepExtend(dest, source) {
+    for (var prop in source) {
+      if (source[prop] && source[prop].constructor &&
+       source[prop].constructor === Object) {
+        dest[prop] = dest[prop] || {};
+        domDeepExtend(dest[prop], source[prop]);
+      } else {
+        dest[prop] = source[prop];
+      }
+    }
+    return dest;
+  }
+};
 
 /*
 *####
@@ -82,6 +230,58 @@ var Fsm = {
   }
 };
 
+/*
+*#####
+*MATH#
+*#####
+*/
+Math.rand = function mathRand(min, max) {
+  return (min + (this.random() * (max - min)));
+};
+Math.randInt = function mathRandInt(min, max) {
+  return Math.round(this.rand(min, max));
+};
+Math.randChoice = function mathRandChoice(choices) {
+  return choices[this.randInt(0, choices.length - 1)];
+};
+Math.randBool = function mathRandBool() {
+  return this.randChoice([true, false]);
+};
+Math.limit = function mathLimit(x, min, max) {
+  return Math.max(min, Math.min(max, x));
+};
+Math.between = function mathBetween(n, min, max) {
+  return ((n >= min) && (n <= max));
+};
+Math.countdown = function mathCountdown(n, dn) {
+  return Math.max(0, n - (dn || 1));
+};
+Math.accel = function mathAccel(v, accel, dt) {
+  return v + (accel * dt);
+};
+Math.lerp = function mathLerp(n, dn, dt) {
+  return n + (dn * dt);
+};
+// easing functions
+Math.easeLerp = function mathEaseLerp(a ,b, percent) {
+  return a + (b - a) * percent;
+};
+Math.easeIn = function mathEaseIn(a, b, percent) {
+  return a + (b - a) * Math.pow(percent, 2);
+};
+Math.easeOut = function mathEaseOut(a, b, percent) {
+  return a + (b - a) * (1 - Math.pow(1 - percent, 2));
+};
+Math.easeInOut = function mathEaseInOut(a,b,percent) {
+  return a + (b - a) * ((-Math.cos(percent * Math.PI)/2) + 0.5);
+};
+Math.collBox = function mathCollBox(x1, y1, w1, h1, x2, y2, w2, h2) {
+  return !(((x1 + w1 - 1) < x2) ||
+           ((x2 + w2 - 1) < x1) ||
+           ((y1 + h1 - 1) < y2) ||
+           ((y2 + h2 - 1) < y1));
+};
+
 var states = {
     events: [
         { name: "startUp", from: "booting", to: "login", action: function Action() { /**/ } },
@@ -101,9 +301,15 @@ var states = {
 
 function startGame() {
     //init FSM
-    //set dates on emails
+    composeEmails(4);
     //set stupid question
     //set password test
+}
+
+function composeEmails(num) {
+    for (i = 0; i < num; i++) {
+
+    }
 }
 
 //assumes months are counted from 1-12 not 0-11
@@ -195,11 +401,35 @@ function startPWD() {
 function startEmail() {
     document.getElementById("mainInterface").innerHTML = temp_thanks;
     document.getElementById("prompt").style.padding = "100px 0px 0px 0px";
-    setTimeout("function spoolEmail() {/*1. set eCount to a visible size, 20px 2. spool email upwards/make it visible from being invisible 3. make sure that when gozilla lightningbird is clicked it slides upwards by calling slideUp()*/}", 1000)
+    setTimeout(function displayEmail() {
+        var i = 0;
+        document.getElementById('emailContOutline').removeAttribute('hidden');
+        var emailSpoolInt = setInterval( function spoolEmail() {
+            document.getElementById('emailCont').style.height = (i++) + 'px';
+            if (i === 30) {
+                clearInterval(emailSpoolInt);
+            }
+        }, 10);
+    }, 1000)
 }
 
-function slideUp() {
-
+function emailSlideUp() {
+    if (gozillaClick === false) {
+        var i = 0;
+        var k = 30;
+        gozillaClick = true;
+        document.getElementById("emailSys").style.cursor = "default";
+        var emailSlideUpInt = setInterval(function emailSlideUpI() {
+            document.getElementById("emailContOutline").style.height = (i += 4) + 'px';
+            document.getElementById("emailCont").style.height = (k += 4) + 'px';
+            if (k > 440) {
+                clearInterval(emailSlideUpInt);
+            }
+        }, 1);
+    }
+    else {
+        //do nothing
+    }
 }
 
 function emailRead(num) {
@@ -252,9 +482,17 @@ function emailBack() {
 }
 
 function startExpired() {
-
+    document.getElementById("emailContOutline").setAttribute("hidden", "true");
+    document.getElementById("mainInterface").innerHTML = temp_expired;
+    document.getElementById("prompt").style.padding = "100px 0px 0px 0px";
 }
 
 function resetGame() {
-
+    gozillaClick = false;
+    newMailRead = false;
+    usedStupidQues = "";
+    usedPassPuzz = "";
+    usedEmails = [];
+    //startGame();
+    // should include a reset mainInterface and a reset emailContent ie: document.getElementById("mainInterface").innerHTML = temp_login; & etc.
 }
