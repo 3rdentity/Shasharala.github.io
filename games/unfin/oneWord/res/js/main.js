50abb524107bf34daa30ef167436abf48178d557
shasharala <shasharala@outlook.com>
2015-03-02 03:14:01 -0800

    moremessymessymessy

--- games/unfin/oneWord/res/js/main.js
+++ games/unfin/oneWord/res/js/main.js
@@ -22,14 +22,12 @@ var temp_pwd = '<div id="titleBar">SuperSecureSite<sup>TM</sup> Authentication</
 var temp_thanks = '<div id="titleBar">SSS<sup>TM</sup> Password Request</div><form id="dataBox"><p id="prompt">Thank you!<br><br>An e-mail has been sent to the e-mail address last associated with your current IP Address.<br><br>If you have any questions or do not receive an e-mail you must visit your nearest SSS establishment for further assistance.</p></form>';
 var temp_expired = '<div id="mainInterface"><div id="titleBar">SSS<sup>TM</sup> Password Reset Expired</div><form id="dataBox" onsubmit="test()"><p id="prompt">It appears that this session has expired.<br><br>We apologize for any inconvenience this has caused you.<br><br>Please <a id="resetLink" onclick="resetGame()">start a new session</a>.<br><br>If you need assistance or have any questions please visit the closest SSS establishment to you.</p></form></div>';
 var temp_tempEmails = '';
-var temp_email = '<img id="eCount" src="res/img/eCount.png"><span id="emailSys">Gozilla Lightningbird<sup>TM</sup></span><hr>';
 
 /*
 *######
 *EMAILS
 *######
 */
-/*+ (((theDay - 1) >= 1) ? (theMonth + "/" + (theDay - 1) + "/" + theShortYear) : ((theMonth - 1) + "/" + (theDay + 1) + "/" + theShortYear)) +*/
 var emails = [
     ["IMPORTANT: SSS Password Reset", "SSS", 0, "Dear userNameHere,<br><br>You are receiving this e-mail because someone (Was it you?) requested a password reset from the IP Address associated with this e-mail in our records.<br>If you did not request a password reset then please visit the closest SSS establishment to you immediatly, your account may be vulnerable.<br><br>If you did request a password resest please click <a id='passResetLink' onclick='startExpired()'>here</a>."],
     ["What's Up?", "Robbin", -2, "Helllooo, anyone in there?<br>I haven't heard from you in a while!<br>What are you up to? Do you not like me anymore or something? I know you're busy with your \"projects\" or whatever, but we havn't hung out in ages! What's up with that?<br>Let's go out somewhere. I don't care where. Let's break something, do something, talk, c'mon, anything! I miss you!<br><br>Call me or something, ASAP, pleaasse?"],
@@ -37,7 +35,7 @@ var emails = [
     ["Ladykiller!!!", "Matthew", -1, "Hey!<br><br>Have you seen <a href='https://www.youtube.com/watch?v=fEW2pdZ0qEE' target='_blank'>this</a> game? It looks amazing!<br>From what I have read about it it seems like it is not only particularly risqu&eacute;, hehe, but it will be very different from traditional visual novels! You know how in most visual novels wooing a specific chracter is done by doing things specifically for the character? Filling them up with charity like a vending machine till their adoration for you falls out? Well, this game ain't gonna take any of that shit from you. Your manipulation will have consequences.<br><br>Sounds exciting, huh?"]
 ];
 
-var usedEmails = [];
+var usedEmails = "";
 
 /*
 *################
@@ -204,33 +202,6 @@ var Dom = {
 };
 
 /*
-*####
-*FSM#
-*####
-*/
-var Fsm = {
-  trans: false, // variable to check that Fsm is transitioning between states
-  init: function fsmInit(obj, cfg) {
-    obj.is = cfg.initial || "booting";
-    var onLeaveState = cfg.onLeaveState || function defOnLeaveState() { Fsm.trans = true; },
-    onEnterState = cfg.onEnterState || function defOnEnterState(i) { setTimeout( function setTimeoutDefOnEnterState() { obj.is = cfg.events[i].to; Fsm.trans = false; }, 0); }; // setTimeout is used to stop synchronous kepresses & events from occuring
-    for (var i = 0; i < cfg.events.length; i++) {
-      obj[cfg.events[i].name] = this.compile(obj, cfg, i, onLeaveState, onEnterState);
-    }
-  },
-  compile: function fsmCompile(obj, cfg, i, onLeaveState, onEnterState) {
-    var func = function fsmEvent() {
-      if (obj.is === cfg.events[i].from && Fsm.trans === false || Is.array(cfg.events[i].from) && cfg.events[i].from.indexOf(obj.is) >= 0 && Fsm.trans === false) {
-        onLeaveState();
-        cfg.events[i].action();
-        onEnterState(i);
-      }
-    };
-    return func;
-  }
-};
-
-/*
 *#####
 *MATH#
 *#####
@@ -282,16 +253,107 @@ Math.collBox = function mathCollBox(x1, y1, w1, h1, x2, y2, w2, h2) {
            ((y2 + h2 - 1) < y1));
 };
 
-var states = {
-    events: [
-        { name: "startUp", from: "booting", to: "login", action: function Action() { /**/ } },
-        { name: "startStupid", from: "login", to: "stupidQues", action: function pauseAction() { /*pause screen here*/ } },
-        { name: "startPWD", from: "stupidQues", to: "pwdPuzzle", action: function unPauseAction() { /*remove pause screen here*/ } },
-        { name: "startEmail", from: "pwdPuzzle", to: "emails", action: function unPauseAction() { /*remove pause screen here*/ } },
-        { name: "startExpired", from: "emails", to: "expired", action: function unPauseAction() { /*remove pause screen here*/ } },
-        { name: "resetGame", from: "expired", to: "login", action: function unPauseAction() { /*remove pause screen here*/ } }
-    ]
-}
+/*
+*########
+*num2Word
+*########
+*/
+var num2Word = {
+    thousandsPlus: [
+        "",
+        "thousand",
+        "million",
+        "billion",
+        "trillion"
+    ],
+    digits: [
+        "zero",
+        "one",
+        "two",
+        "three",
+        "four",
+        "five",
+        "six",
+        "seven",
+        "eight",
+        "nine"
+    ],
+    teens:[
+        "ten",
+        "eleven",
+        "twelve",
+        "thirteen",
+        "fourteen",
+        "fifteen",
+        "sixteen",
+        "seventeen",
+        "eighteen",
+        "nineteen"
+    ],
+    twentyPlus: [
+        "twenty",
+        "thirty",
+        "forty",
+        "fifty",
+        "sixty",
+        "seventy",
+        "eighty",
+        "ninety"
+    ],
+    conv: function num2Word(num) {
+        num = num.toString();
+        num = num.replace(/[\, ]/g,""); //get rid of spaces or commas globally
+        if (num != parseFloat(num)) return "NAN"; //make sure num is actually a number
+        var x = num.indexOf("."); //holds length of num or length/indexOf a decimal point
+        if (x == -1) {
+            x = num.length;
+        }
+        if (x > 15) {
+            return "The number you have provided is too large";
+        }
+        var n = num.split(""); //holds num broken into an array of integers
+        var res = ""; //holds the result of our conversion
+        var rev = false; //is true if current digit has been reviewed already
+        if (x === 1 && Number(n[0]) === 0) { //if input is zero there is no need to do very many tests, just return zero
+            res += this.digits[0];
+            return res;
+        }
+        else {
+            for (var i = 0; i < x; i++) { //convert until we reach a decimal point or the end of n
+                if ((x - i) % 3 === 2) { //if x has a remainder of two it must be a teens number, a twentyPlus number, or a zero
+                    if (Number(n[i]) === 1) { //if x has a remainder of two and n starts with "1" then n must be a teens number
+                        res += this.teens[Number(n[i + 1])] + " "; //find teens number in array by checking number of next digit in n
+                        i++; //move i forward one since the teens array contains numbers that cover two digits and we already checked the next digit in n
+                        rev = true;
+                    }
+                    else if (Number(n[i]) !== 0) { //if n is not zero the it must be a twentyPlus number
+                        res += this.twentyPlus[Number(n[i]) - 2] + " "; //find twentyPlus number in array by stepping back two since array starts at twenty with an index of zero
+                        rev = true;
+                    }
+                }
+                else if (Number(n[i]) !== 0) { //if n is does not have a remainder of two then it must be either a single digit or a hundreds number
+                    res += this.digits[Number(n[i])] +" "; //find digit in array
+                    if ((x - i) % 3 === 0) res += "hundred "; //if x has no remained then it is a hundreds number. add the word "hundred" after the digit we just added
+                    rev = true;
+                }
+                if ((x - i) % 3 === 1) { //if x has a remainder of one then it must be a thousandsPlus number
+                    if (rev === true) { //if n has already been reviewed and must be a thousandsPlus number because of the above text then we may continue
+                        res += this.thousandsPlus[(x - (i + 1)) / 3] + " "; //locate what thousandths place is being referenced by taking the length of num and removing (current index + 1)(to account for the zero index in the thousandsPlus array) from it before dividing it by three
+                    }
+                    rev = false;
+                }
+            }
+            if (x !== num.length) { //if x is the indexOf a decimal point
+                var y = num.length;
+                res += "point ";
+                for (var i = x + 1; i < y; i++) { //i will start at x and move along till it reaches the end of n
+                    res += this.digits[Number(n[i])] + " "; //find digit in array
+                }
+            }
+            return res;
+        }
+    }
+};
 
 /*
 *#########
@@ -300,75 +362,52 @@ var states = {
 */
 
 function startGame() {
-    //init FSM
-    composeEmails(4);
+    composeEmails(3);
     //set stupid question
     //set password test
 }
 
 function composeEmails(num) {
-    for (i = 0; i < num; i++) {
-
+    document.getElementById("emailsTable").innerHTML += '<tr id="emailNew" class="emails" onclick="emailRead(0)"><td>IMPORTANT: SSS Password Reset</td><td>SSS</td></tr>';
+    for (i = 0; i < num; i++) { //compose emails in relation to number requested
+        var chosenEmailNum = Math.randInt(1, (emails.length - 1));
+        var chosenEmailStr = num2Word.conv(chosenEmailNum);
+        while (Dom.searchStr(usedEmails, chosenEmailStr) !== -1) { //while chosen email has been chosen already then
+            chosenEmailNum = Math.randInt(1, (emails.length - 1));
+            chosenEmailStr = num2Word.conv(chosenEmailNum);
+        }
+        usedEmails += chosenEmailStr + ", ";
+        document.getElementById("emailsTable").innerHTML += '<tr class="emails" onclick="emailRead(' + (chosenEmailNum) + ')"><td>' + (emails[chosenEmailNum][0]) + '</td><td>' + (emails[chosenEmailNum][1]) + '</td><td>' + (((theDay + (emails[chosenEmailNum][2])) >= 1) ? (theMonth + "/" + (theDay + (emails[chosenEmailNum][2])) + "/" + theShortYear) : ((theMonth - 1) + "/" + (theDay - (emails[chosenEmailNum][2])) + "/" + theShortYear)) + '</td></tr>';
     }
 }
 
-//assumes months are counted from 1-12 not 0-11
+//assumes months are not being counted with zero-based numbering
 function month2Month(val) {
-    if (val > 0) {
-        switch (val) {
-            case 1 || "01":
-                return "January";
-            case 2 || "02":
-                return "February";
-            case 3 || "03":
-                return "March";
-            case 4 || "04":
-                return "April";
-            case 5 || "05":
-                return "May";
-            case 6 || "06":
-                return "June";
-            case 7 || "07":
-                return "July";
-            case 8 || "08":
-                return "August";
-            case 9 || "09":
-                return "September";
-            case 10 || "10":
-                return "October";
-            case 11 || "10":
-                return "Novemver";
-            case 12 || "12":
-                return "December";
-        }
-    }
-    else {
-        switch (val) {
-            case "January" || "Jan" || "JAN":
-                return "01";
-            case "Febuary" || "Feb" || "FEB":
-                return "02";
-            case "March" || "Mar" || "MAR":
-                return "03";
-            case "April" || "Apr" || "APR":
-                return "04";
-            case "May" || "May" || "MAY":
-                return "05";
-            case "June" || "Jun" || "JUNE":
-                return "06";
-            case "July" || "Jul" || "JUL":
-                return "07";
-            case "August" || "Aug" || "AUG":
-                return "08";
-            case "September" || "Sep" || "SEP":
-                return "09";
-            case "October" || "Oct" || "OCT":
-                return "10";
-            case "November" || "Nov" || "NOV":
-                return "11";
-            case "December" || "Dec" || "DEC":
-                return "12";
-        }
+    switch (val) {
+        case 1 || "01":
+            return "January";
+        case 2 || "02":
+            return "February";
+        case 3 || "03":
+            return "March";
+        case 4 || "04":
+            return "April";
+        case 5 || "05":
+            return "May";
+        case 6 || "06":
+            return "June";
+        case 7 || "07":
+            return "July";
+        case 8 || "08":
+            return "August";
+        case 9 || "09":
+            return "September";
+        case 10 || "10":
+            return "October";
+        case 11 || "10":
+            return "Novemver";
+        case 12 || "12":
+            return "December";
     }
 }
 
@@ -432,41 +471,26 @@ function emailSlideUp() {
     }
 }
 
+//<div class='emailContent'><img id='backBtn' onclick='emailBack()' src='res/img/back.png'>&nbsp;<span class='emailContentHeader'><b>Subject:</b> SUBJECT_HERE <b>From:</b>  <b>Date:</b> " CALCS_HERE "</span><hr>CONTENT_HERE</div>
 function emailRead(num) {
     temp_tempEmails = document.getElementById("emailCont").innerHTML;
-    switch (num) {
-        case 0:
-            document.getElementById("emailCont").innerHTML = temp_email + emails[0];
+    var temp_email = '<img id="eCount" src="res/img/eCount.png"><span id="emailSys">Gozilla Lightningbird<sup>TM</sup></span><hr><div class="emailContent"><img id="backBtn" onclick="emailBack()" src="res/img/back.png">&nbsp;<span class="emailContentHeader"><b>Subject:</b>&nbsp;' + (emails[num][0]) + '&nbsp;<b>From:</b>&nbsp;' + (emails[num][1]) + '&nbsp;<b>Date:</b>&nbsp;' + (((theDay + (emails[num][2])) >= 1) ? (theMonth + "/" + (theDay + (emails[num][2])) + "/" + theShortYear) : ((theMonth + (emails[num][2])) + "/" + (theDay - (emails[num][2])) + "/" + theShortYear)) + '&nbsp;</span><hr>' + (emails[num][3]) + '</div>';
+    if (num === 0) {
+        document.getElementById("emailCont").innerHTML = temp_email;
+        document.getElementById("eCount").style.width = "0px";
+        newMailRead = true;
+        document.getElementById("emailSys").style.cursor = "default";
+    }
+    else {
+        if (newMailRead === true) {
+            document.getElementById("emailCont").innerHTML = temp_email;
             document.getElementById("eCount").style.width = "0px";
-            newMailRead = true;
-            break;
-        case 1:
-            if (newMailRead === true) {
-                document.getElementById("emailCont").innerHTML = temp_email + emails[1];
-                document.getElementById("eCount").style.width = "0px";
-            }
-            else {
-                document.getElementById("emailCont").innerHTML = temp_email + emails[1];
-            }
-            break;
-        case 2:
-            if (newMailRead === true) {
-                document.getElementById("emailCont").innerHTML = temp_email + emails[2];
-                document.getElementById("eCount").style.width = "0px";
-            }
-            else {
-                document.getElementById("emailCont").innerHTML = temp_email + emails[2];
-            }
-            break;
-        case 3:
-            if (newMailRead === true) {
-                document.getElementById("emailCont").innerHTML = temp_email + emails[3];
-                document.getElementById("eCount").style.width = "0px";
-            }
-            else {
-                document.getElementById("emailCont").innerHTML = temp_email + emails[3];
-            }
-            break;
+            document.getElementById("emailSys").style.cursor = "default";
+        }
+        else {
+            document.getElementById("emailCont").innerHTML = temp_email;
+            document.getElementById("emailSys").style.cursor = "default";
+        }
     }
 }
 
