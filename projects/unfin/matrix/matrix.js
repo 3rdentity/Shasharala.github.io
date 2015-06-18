fdb451740d168ed45edd7670b9e2e7c83e1101b7
shasharala <shasharala@outlook.com>
2015-06-17 20:16:03 -0700

    matrix updates

--- projects/unfin/matrix/matrix.js
+++ projects/unfin/matrix/matrix.js
@@ -3,7 +3,6 @@
  *MATRIX
  *######
  */
-//relies on tween.js
 // tween.js - http://github.com/sole/tween.js - Licensed under the MIT License
 'use strict';void 0===Date.now&&(Date.now=function(){return(new Date).valueOf()});
 var TWEEN=TWEEN||function(){var a=[];return{REVISION:"14",getAll:function(){return a},removeAll:function(){a=[]},add:function(c){a.push(c)},remove:function(c){c=a.indexOf(c);-1!==c&&a.splice(c,1)},update:function(c){if(0===a.length)return!1;for(var b=0,c=void 0!==c?c:"undefined"!==typeof window&&void 0!==window.performance&&void 0!==window.performance.now?window.performance.now():Date.now();b<a.length;)a[b].update(c)?b++:a.splice(b,1);return!0}}}();
@@ -55,6 +54,188 @@ var Color = {
          n = n.toString(16);
         }
     }
+};
+/*
+*####
+*DOM#
+*####
+*/
+var Dom = {
+  type: function domType(obj) {
+    return Object.prototype.toString.call(obj);
+  },
+  searchStr: function domSearchString(str, name) {
+    return str.indexOf(name);
+  },
+  get: function domGet(id) {
+    return document.getElementById(id);
+  },
+  set: function domSet(elem, html) {
+    elem.innerHTML = html;
+  },
+  hasClass: function domHasClass(elem, name) {
+    return (' ' + elem.className + ' ').indexOf(' ' + name + ' ') != -1;
+  },
+  addClass: function domAddClass(elem, name) {
+    elem.toggleClassName(name, true);
+  },
+  remClass: function domRemClass(elem, name) {
+    elem.toggleClassName(name, false);
+  },
+  toggleClass: function domToggleClass(elem, name, on) {
+    var classes = elem.className.split(' ');
+    var n = classes.indexOf(name);
+    on = (typeof on == "undefined") ? (n < 0) : on;
+    if (on && (n < 0)) classes.push(name);
+    else if (!on && (n >= 0)) classes.splice(n, 1);
+    elem.className = classes.join(' ');
+  },
+  hasAttr: function domHasAttr(elem, attr) {
+    return elem.hasAttribute(attr);
+  },
+  getAttr: function domGetAttr(elem, attr) {
+    return elem.getAttribute(attr);
+  },
+  addAttr: function domAddAttr(elem, attr, val) {
+    elem.setAttribute(attr, val);
+  },
+  remAttr: function domRemAttr(elem, attr) {
+    elem.removeAttribute(attr);
+  },
+  query: function domQuery(selector, context) {
+    return (context || document).querySelectorAll(selector);
+  },
+  on: function domOn(on, elem, type, fn, capture) {
+    capture = (typeof capture == "undefined") ? false : capture;
+    if(on === "elem") document.getElementById(elem).addEventListener(type, fn, capture);
+    else if(on === "obj") elem.addEventListener(type, fn, capture);
+  },
+  un: function domUn(elem, type, fn, capture) {
+    capture = (typeof capture == "undefined") ? false : capture;
+    document.getElementById(elem).removeEventListener(type, fn, capture);
+  },
+  // add "[hidden] {display: none;}" to CSS as a fallback
+  hide: function domHide(elem) {
+    this.addAttr(elem, "hidden");
+  },
+  show: function domShow(elem) {
+    this.remAttr(elem, "hidden");
+  },
+  replace: function domReplace(name1, name2) {
+    this.hide(name1);
+    this.show(name2);
+  },
+  cancelFade: function domCancelFade(elem) {
+    if (elem.fading) delete elem.fading;
+  },
+  fadeOut: function domFadeOut(elem, time) {
+    var fadeTime = 16;
+    if (time) fadeTime = time;
+    var opacity = 1;
+    elem.fading = undefined;
+    elem.style.opacity = 1;
+    elem.style.filter = "";
+    var last = new Date.getTime();
+    var tick = function fadeOutTick() {
+      opacity -= (new Date.getTime() - last) / 400;
+      elem.style.opacity = opacity;
+      elem.style.filter = "alpha(opacity=" + (100 * opacity)|0 + ")";
+      last = new Date.getTime();
+      (opacity > 0 && elem.fading) ? setTimeout(tick, fadeTime) : this.hide(elem);
+    };
+    tick();
+  },
+  fadeIn: function domFadeIn(elem, time) {
+    var fadeTime = 16;
+    if (time) fadeTime = time;
+    var opacity = 0;
+    elem.fading = undefined;
+    elem.style.opacity = 0;
+    elem.style.filter = "";
+    var last = new Date.getTime();
+    var tick = function fadeInTick() {
+      opacity += (new Date.getTime() - last) / 400;
+      elem.style.opacity = opacity;
+      elem.style.filter = "alpha(opacity=" + (100 * opacity)|0 + ")";
+      last = new Date.getTime();
+      if (opacity < 1 && elem.fading) setTimeout(tick, fadeTime);
+    };
+    this.show(elem);
+    tick();
+  },
+  replaceFade: function domReplaceFade(elem1, elem2, time1, time2) {
+    //fadeIn & fadeOut will handle time1 & time2 if they are not specified
+    this.fadeOut(elem1, time1);
+    this.fadeIn(elem2, time2);
+  },
+  // props may be overwrit and modifying extended obj will affect orig obj
+  // caveats:
+  // - objects from other frames/pages will be copied by reference, because their version of Object will be different
+  // - objects with a cyclic structure, will be traversed forever and overflow the JS stack
+  extend: function domExtend(dest, source) {
+    for (var prop in source)
+      dest[prop] = source[prop];
+    return dest;
+  },
+  // deepExtend will extend the object so that props are not overwrit and modifying extended obj won't affect orig obj
+  // caveats:
+  // - objects from other frames/pages will be copied by reference, because their version of Object will be different
+  // - objects with a cyclic structure, will be traversed forever and overflow the JS stack
+  deepExtend: function domDeepExtend(dest, source) {
+    for (var prop in source) {
+      if (source[prop] && source[prop].constructor &&
+       source[prop].constructor === Object) {
+        dest[prop] = dest[prop] || {};
+        domDeepExtend(dest[prop], source[prop]);
+      } else {
+        dest[prop] = source[prop];
+      }
+    }
+    return dest;
+  }
+};
+/*
+*#######
+*LOADING
+*#######
+*/
+function loading(size, color) {
+    var canvas = document.getElementById('loading') || null;
+    canvas ? canvas.ctx = canvas.getContext('2d') : null;
+    var circles = 12;
+    var size = size || 128;
+    var color = color || '242, 242, 242';
+    if (!canvas) {
+        var canvas = document.createElement('canvas');
+        canvas.ctx = canvas.getContext('2d');
+        canvas.id = 'loading';
+        canvas.width = size;
+        canvas.height = size;
+        canvas.style.position = 'absolute';
+        canvas.style.top = '45%';
+        canvas.style.left = '50%';
+        canvas.style.marginLeft = '-' + size / 3 + 'px'; // to position correctly on screen
+        canvas.style.backgroundColor = 'rgba(1, 1, 1, 0.4)';
+        canvas.style.borderRadius = '8px';
+        document.body.appendChild(canvas);
+    }
+
+    canvas.ctx.translate(size / 2, size / 2);
+
+    function run() {
+        canvas.ctx.clearRect(-size / 2, -size / 2, size, size);
+        canvas.ctx.rotate(Math.PI * 2 / circles);
+        for (var i = 0; i < circles; i++) {
+            canvas.ctx.beginPath();
+            canvas.ctx.arc(0, size / 4, size / 32, 0, 2 * Math.PI, false);
+            canvas.ctx.fillStyle = 'rgba(' + color + ', ' + i / circles + ')';
+            canvas.ctx.fill();
+            canvas.ctx.closePath();
+            canvas.ctx.rotate(Math.PI * 2 / circles);
+        }
+        requestAnimationFrame(run);
+    }
+    requestAnimationFrame(run);
 };
 /*
 *#####
@@ -146,11 +327,6 @@ var Matrix = function matrix() {
     var points = [];
     var stopped = false;
     var animate = true;
-    function loading() {
-        ctx.clearRect(0, 0, canvas.width, canvas.height);
-        ctx.
-        return this;
-    };
     function stop() {
         stopped = true;
         remListeners();
@@ -264,8 +440,7 @@ var Matrix = function matrix() {
             ctx.restore();
         }
     }
-    function circle(pos, rad) {
-        var that = this;
+    function circle(pos, rad, opacity) {
         this.pos = pos || null;
         this.rad = rad || null;
         this.draw = function shapeCircleDraw() {
@@ -274,15 +449,15 @@ var Matrix = function matrix() {
             }
             ctx.save();
             ctx.beginPath();
-            ctx.arc(that.pos.x, that.pos.y, that.rad, 0, 2 * Math.PI, false);
-            ctx.fillStyle = "rgba(" + pColor + ", " + this.active + ")";
+            ctx.arc(this.pos.x, this.pos.y, this.rad, 0, 2 * Math.PI, false);
+            if (opacity) ctx.fillStyle = "rgba(" + pColor + ", " + opacity + ")";
+            else ctx.fillStyle = "rgba(" + pColor + ", " + this.active + ")";
             ctx.fill();
             ctx.closePath();
             ctx.restore();
         };
     }
     function star(pos, rad, inset, p) {
-        var that = this;
         this.pos = pos || null;
         this.rad = rad || null;
         this.inset = inset || null;
@@ -293,13 +468,13 @@ var Matrix = function matrix() {
             }
             ctx.save();
             ctx.beginPath();
-            ctx.translate(that.pos.x, that.pos.y);
-            ctx.moveTo(0, 0 - that.rad) // moveTo that.pos.x & that.pos.y but on the outer radius/the first point of the star
-            for (i = 0; i < that.points; i++) {
-                ctx.rotate(Math.PI / that.points);
-                ctx.lineTo(0, 0 - (that.rad * that.inset));
-                ctx.rotate(Math.PI / that.points);
-                ctx.lineTo(0, 0 - that.rad);
+            ctx.translate(this.pos.x, this.pos.y);
+            ctx.moveTo(0, 0 - this.rad) // moveTo this.pos.x & this.pos.y but on the outer radius/the first point of the star
+            for (i = 0; i < this.points; i++) {
+                ctx.rotate(Math.PI / this.points);
+                ctx.lineTo(0, 0 - (this.rad * this.inset));
+                ctx.rotate(Math.PI / this.points);
+                ctx.lineTo(0, 0 - this.rad);
             }
             ctx.fillStyle = "rgba(" + pColor + ", " + this.active + ")";
             ctx.fill();
@@ -308,21 +483,20 @@ var Matrix = function matrix() {
         };
     }
     function heart(pos, rad) {
-        var that = this;
         this.pos = pos || null;
         this.rad = rad || null;
         this.draw = function shapeHeartDraw() {
             ctx.save();
             ctx.beginPath();
-            ctx.translate(that.pos.x, that.pos.y);
+            ctx.translate(this.pos.x, this.pos.y);
             ctx.rotate(4); //position heart correctly
-            ctx.moveTo(-that.rad, 0);
-            ctx.arc(0, 0, that.rad, 0, Math.PI, false); // first/left heart arc
-            ctx.lineTo(that.rad, 0); // line to first/left heart arc
-            ctx.arc(that.rad, -that.rad, that.rad, Math.PI * 90 / 180, Math.PI * 270 / 180, true);
-            ctx.lineTo(that.rad, -that.rad * 2);
-            ctx.lineTo(-that.rad, -that.rad * 2);
-            ctx.lineTo(-that.rad, 0);
+            ctx.moveTo(-this.rad, 0);
+            ctx.arc(0, 0, this.rad, 0, Math.PI, false);
+            ctx.lineTo(this.rad, 0);
+            ctx.arc(this.rad, -this.rad, this.rad, Math.PI * 90 / 180, Math.PI * 270 / 180, true);
+            ctx.lineTo(this.rad, -this.rad * 2);
+            ctx.lineTo(-this.rad, -this.rad * 2);
+            ctx.lineTo(-this.rad, 0);
             ctx.fillStyle = "rgba(" + pColor + ", " + this.active + ")";
             ctx.fill();
             ctx.closePath();
