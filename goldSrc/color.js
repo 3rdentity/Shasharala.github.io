9010e261f83896cf0a4ea59fd4a7e274b5de17c6
shasharala <shasharala@outlook.com>
2016-11-09 03:55:15 +0000

    more stuff for the about page!

--- goldSrc/color.js
+++ goldSrc/color.js
@@ -23,14 +23,15 @@ var Color = {
         }
     },
     rgb2Hex: function rgb2Hex(r, g, b, h) {
+        function toHex(n) {
+            hexString = "0123456789ABCDEF";
+            return String(hexString.substr((n >> 4) & 0x0F,1)) + hexString.substr(n & 0x0F,1);
+        }
         if (h) {
             return "#" + toHex(r) + toHex(g) + toHex(b);
         }
         else {
-            return toHex(r) + toHex(g) + toHex(b);
-        }
-        function toHex(n) {
-         n = n.toString(16);
+            return "#" + toHex(r) + toHex(g) + toHex(b);
         }
     }
 };
